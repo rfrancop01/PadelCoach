7ad494348ebe27905ff54715374824b8523e4ef5
Ricardo Franco <rfrancop01@gmail.com>
2025-04-02 15:18:14 +0200

    changes added

--- server/config.py
+++ server/config.py
@@ -6,4 +6,8 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MIGRATION_DIRECTORY = 'server/migrations' 
+    MIGRATION_DIRECTORY = 'server/migrations'
+    JWT_TOKEN_LOCATION = ['headers']
+    JWT_HEADER_NAME = 'Authorization'
+    JWT_HEADER_TYPE = 'Bearer'
+    JWT_SECRET_KEY = 'tu_clave_secreta_aqui'
